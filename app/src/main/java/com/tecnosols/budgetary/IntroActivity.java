c2824f1db9863606b2c9de6890fc7a0bd1b080a0
Deepak Singh Baise <deepaksinghbaise@gmail.com>
2020-02-13 01:59:37 +0530

    added intro and splash activity

---
+++ app/src/main/java/com/tecnosols/budgetary/IntroActivity.java
@@ -0,0 +1,134 @@
+package com.tecnosols.budgetary;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IntroActivity extends AppCompatActivity {
+
+    private ViewPager screenPager;
+    IntroViewPagerAdapter introViewPagerAdapter;
+    TabLayout tabIndicator;
+    Button btnNext;
+    int position = 0;
+    Button btnGetStarted;
+    Animation btnAnim;
+    TextView tvSkip;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_intro);
+
+        btnNext = findViewById(R.id.btn_next);
+        btnGetStarted = findViewById(R.id.btn_get_started);
+        tabIndicator = findViewById(R.id.tab_indicator);
+        btnAnim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.button_animation);
+        tvSkip = findViewById(R.id.tv_skip);
+
+        String mExp="Manage your monthly expenses and track down the most expensive day of the month via graph plotted on the daily basis.";
+        String dExp="Track down your daily expenses at the end of the day or whenever you want to, what and how much you spend on could be easily tracked.";
+        String aExp="Add your daily expenses totrack them later. Data is stored securily in the cloud so add everything without thinking much about security.";
+
+        final List<ScreenItem> mList = new ArrayList<>();
+        mList.add(new ScreenItem("Monthly Expenses", mExp, R.drawable.me));
+        mList.add(new ScreenItem("Daily Expenses", dExp, R.drawable.de));
+        mList.add(new ScreenItem("Add Expenses", aExp, R.drawable.ae));
+
+        // setup viewpager
+        screenPager = findViewById(R.id.screen_viewpager);
+        introViewPagerAdapter = new IntroViewPagerAdapter(this, mList);
+        screenPager.setAdapter(introViewPagerAdapter);
+
+        // setup tablayout with viewpager
+        tabIndicator.setupWithViewPager(screenPager);
+
+        // next button click Listner
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                position = screenPager.getCurrentItem();
+
+                if (position < mList.size()) {
+                    position++;
+                    screenPager.setCurrentItem(position);
+                }
+                if (position == mList.size() - 1) {
+                    loaddLastScreen();
+
+                }
+            }
+        });
+
+        tabIndicator.addOnTabSelectedListener(new TabLayout.BaseOnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+
+                if (tab.getPosition() == mList.size() - 1) {
+                    loaddLastScreen();
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        btnGetStarted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent mainActivity = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(mainActivity);
+            }
+        });
+
+        // skip button click listener
+        tvSkip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                screenPager.setCurrentItem(mList.size());
+            }
+        });
+
+    }
+
+    // show the GETSTARTED Button and hide the indicator and the next button
+    private void loaddLastScreen() {
+
+        btnNext.setVisibility(View.INVISIBLE);
+        btnGetStarted.setVisibility(View.VISIBLE);
+        tvSkip.setVisibility(View.INVISIBLE);
+        tabIndicator.setVisibility(View.INVISIBLE);
+
+        // setup animation
+        btnGetStarted.setAnimation(btnAnim);
+
+    }
+}
